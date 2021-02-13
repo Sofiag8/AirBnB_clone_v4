621596644cb8f01b28ad274e50b5bf415e8642c6
serosc95 <oscardleon95@gmail.com>
2021-02-13 21:31:25 +0000

    add script and remove amenity of list

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,13 +1,15 @@
 const $ = window.$;
 $(document).ready(function () {
-  const listCheck = [];
+  let listCheck = [];
+  const indexList;
   $('input:checkbox').change(function () {
     if ($(this).is(':checked')) {
       console.log('checkVal  ', $(this).attr('data-id'));
       listCheck.push($(this).attr('data-id'));
     } else {
       console.log('descheckVal  ', $(this).attr('data-id'));
-      listCheck.removeItem$($(this).attr('data-id'));
+      indexList = listCheck.indexOf($(this).attr('data-id'));
+      (indexList > -1) ? listCheck.splice(indexList, 1);
     }
     console.log('List  ', listCheck);
   });
