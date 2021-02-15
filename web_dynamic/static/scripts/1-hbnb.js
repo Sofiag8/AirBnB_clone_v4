e6b0f96c7e54357f485ae3ad186e3e447596821a
Sofiag8 <1943@holbertonschool.com>
2021-02-15 19:23:11 +0000

    changes

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,15 +1,14 @@
-const $ = window.$;
 $(document).ready(function () {
-  let listCheck = [];
-  const indexList;
-  $('input:checkbox').change(function () {
+  const listCheck = [];
+  const indexList = [];
+  $('input:checkbox').click(function () {
     if ($(this).is(':checked')) {
       console.log('checkVal  ', $(this).attr('data-id'));
       listCheck.push($(this).attr('data-id'));
     } else {
       console.log('descheckVal  ', $(this).attr('data-id'));
       indexList = listCheck.indexOf($(this).attr('data-id'));
-      (indexList > -1) ? listCheck.splice(indexList, 1);
+      listCheck.splice(indexList, 1);
     }
     console.log('List  ', listCheck);
   });
