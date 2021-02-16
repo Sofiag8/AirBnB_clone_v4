dbf9037e7592dfa12795f07b8ac70a07c7f48e59
serosc95 <oscardleon95@gmail.com>
2021-02-16 01:15:37 +0000

    styles amenities and add checklist

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -8,14 +8,15 @@ $(document).ready(function () {
     } else {
       delete listCheck[id];
     }
-<<<<<<< HEAD
-    console.log(listCheck);
-=======
-    const list = [];
-    for (name in listCheck) {
-	list.push(listCheck[name]);
+    $('div.amenities h4').text('');
+    for (id in listCheck) {
+      if ($('div.amenities h4').width() < 215px){
+        $('div.amenities h4').append(listCheck[id]);
+        $('div.amenities h4').append(', ');
+      } else {
+        $('div.amenities h4').append('...');
+        break;
+      }
     }
-    $('div.amenities h4').text(list.join(', '));
->>>>>>> f21f2dcc949f8d63d5a6cdb2f1474937f8a58c44
   });
 });
