e3502c946a06e32eeec542e75faf1f91aff445e2
serosc95 <oscardleon95@gmail.com>
2021-02-16 03:58:04 +0000

    places articles API

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,53 @@
+$(document).ready(function () {
+  let listCheck = {};
+  $('input:checkbox').change(function () {
+    let id =  $(this).attr('data-id');
+    let name =  $(this).attr('data-name');
+    if ($(this).is(':checked')) {
+      listCheck[id] = name;
+    } else {
+      delete listCheck[id];
+    }
+    $('div.amenities h4').text('');
+    for (id in listCheck) {
+      if ($('div.amenities h4').width() < 215){
+        $('div.amenities h4').append(listCheck[id]);
+        $('div.amenities h4').append(', ');
+      } else {
+        $('div.amenities h4').append('...');
+        break;
+      }
+    }
+  });
+
+  $.ajax({
+    url: 'http://localhost:5001/api/v1/status/',
+    //type: 'GET',
+    //dataType: 'text',
+    success: function (data) {
+	console.log(data);
+      if (data.status === 'OK') {
+        $('div#api_status').addClass('available');
+      } else {
+        $('div#api_status').removeClass('available');
+      }
+    },
+    error: function (error) {
+      console.log('error', error);
+    }
+  });
+
+  $.ajax({
+    url: 'http://0.0.0.0:5001/api/v1/places_search/',
+    type: 'POST',
+    contentType: 'application/json',
+    data: {},
+    dataType: 'json',
+    success: function (data) {
+        console.log(data);
+    },
+    error: function (error) {
+      console.log('error', error);
+    }
+  });
+});
