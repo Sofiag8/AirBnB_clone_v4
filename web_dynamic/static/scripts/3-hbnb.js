77a84cc11d3c57fd70a1543f230d21756e237164
serosc95 <oscardleon95@gmail.com>
2021-02-17 01:37:29 +0000

    article places

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -46,17 +46,15 @@ $(document).ready(function () {
     success: data => {
 	data.map(place => {
 	    $('section.places').append(`
-		    <article class="title_box">
-		    <h2>${place.name}</h2>
-		    </div>
-		    <div class="price_by_night">${place.price_by_night}</div>
-		    </div>
+                    <article>
+                    <div class='title_box'>
+                    <h2>#${place.name}</h2>
+                    <div class="price_by_night">${place.price_by_night}</div>  
+                    </div>
 		    <div class="information">
 	            <div class="max_guest">${place.max_guest} Guest${place.max_guest != 1 ? 's' : ''} </div>
 		    <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms != 1 ? 's' : ''}</div>
 		    <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms != 1 ? 's' : ''}</div>
-		    </div>
-		    <div class="user">
 		    </div>
 		    <div class="description">
 	            <p>${place.description}<p>
