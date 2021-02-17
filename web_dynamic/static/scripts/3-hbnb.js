7211a8789144828e7a2111b273c5216c48b7ee79
serosc95 <oscardleon95@gmail.com>
2021-02-17 01:41:11 +0000

    article places

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -48,7 +48,7 @@ $(document).ready(function () {
 	    $('section.places').append(`
                     <article>
                     <div class='title_box'>
-                    <h2>#${place.name}</h2>
+                    <h2>${place.name}</h2>
                     <div class="price_by_night">${place.price_by_night}</div>  
                     </div>
 		    <div class="information">
